f8e682b7798d8d42f4e51b27a1bce267b6119bdf
Ebi <9731002@ceit.local>
2019-06-05 02:37:08 +0430

    class Color is made and is going to have a static method which gives the needed color

--- src/GUI/MainFrame.java
+++ src/GUI/MainFrame.java
@@ -1,34 +1,36 @@
 package GUI;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class MainFrame{
+public class MainFrame {
     private JFrame mainFrame;
-    private static final int HEIGHT = 920,WIDTH=1500;
+    private JPanel panel, panel5, panel2, panel3, panel4;
+    private PlayList playListPanel;
+    private static final int HEIGHT = 920, WIDTH = 1500;
+
     public MainFrame() {
+        //set JFrame's default properties
         this.mainFrame = new JFrame("Spotify");
         mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         mainFrame.setVisible(true);
-        mainFrame.setSize(WIDTH,HEIGHT);
+        mainFrame.setSize(WIDTH, HEIGHT);
 
-        JPanel panel,panel1, panel2, panel3, panel4, panel5;
+        //set Main Panel default properties
         panel = new JPanel();
-        panel1 = new JPanel();
+        panel.setLayout(new BorderLayout(5, 5));
+        panel.setOpaque(true);
+        panel.setBackground(Color.BLACK);
+
         panel2 = new JPanel();
         panel3 = new JPanel();
         panel4 = new JPanel();
         panel5 = new JPanel();
 
-        panel.setLayout(new BorderLayout(5,5));
-        panel.setOpaque(true);
-        panel.setBackground(Color.BLACK);
-
-        panel1.setOpaque(true);
-        panel1.setBackground(Color.ORANGE);
-        panel.setPreferredSize(new Dimension(100,100));
+        playListPanel = new PlayList();
 
         panel2.setOpaque(true);
-        panel2.setBackground(new Color(30,30,30));
+        panel2.setBackground(new Color(30, 30, 30));
 
         panel3.setOpaque(true);
         panel3.setBackground(Color.CYAN);
@@ -39,18 +41,19 @@ public class MainFrame{
         panel5.setOpaque(true);
         panel5.setBackground(Color.PINK);
 
-        panel.add(panel1,BorderLayout.NORTH);
-        panel.add(panel2,BorderLayout.CENTER);
-        panel.add(panel3,BorderLayout.EAST);
-        panel.add(panel4,BorderLayout.WEST);
-        panel.add(panel5,BorderLayout.SOUTH);
+        panel.add(playListPanel, BorderLayout.WEST);
+//        panel.add(panel2,BorderLayout.WEST);
+        panel.add(panel2, BorderLayout.CENTER);
+        panel.add(panel3, BorderLayout.EAST);
+        panel.add(panel4, BorderLayout.NORTH);
+        panel.add(panel5, BorderLayout.SOUTH);
 
         mainFrame.add(panel);
 
     }
 
     public static void main(String[] args) {
-        MainFrame mainFrame=new MainFrame();
+        MainFrame mainFrame = new MainFrame();
     }
 
 
--- src/GUI/PlayList.java
+++ src/GUI/PlayList.java
@@ -1,8 +1,28 @@
-package GUI
+package GUI;
 import javax.swing.*;
+import java.awt.*;
 
 public class PlayList extends JPanel {
+    private GridBagConstraints gbc ;
     public PlayList() {
-        JTextField 
+        super();
+        setLayout(new GridBagLayout());
+        setOpaque(true);
+        setBackground(new Color(20,20,20));
+        gbc = new GridBagConstraints();
+
+        JTextField yourLibrary = new JTextField();
+        yourLibrary.setVisible(true);
+        yourLibrary.setEditable(false);
+        yourLibrary.setSize(100,20);
+        yourLibrary.setText("YOUR LIBRARY");
+        yourLibrary.setBackground(new Color(30,30,30));
+        yourLibrary.setForeground(Color.ORANGE);
+        yourLibrary.setBorder(BorderFactory.createLineBorder(new Color(30,30,30)));
+
+        gbc.gridx=0;
+        gbc.gridy=0;
+        add(yourLibrary,gbc);
+        add(yourLibrary,gbc);
     }
 }
