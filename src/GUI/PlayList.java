cd09d30a25b220be18ceb8efd0a45990124b8832
Ebi <9731002@ceit.local>
2019-06-05 05:22:05 +0430

    JList and button for new playlist are added

--- src/GUI/PlayList.java
+++ src/GUI/PlayList.java
@@ -1,44 +1,73 @@
 package GUI;
 
-import org.w3c.dom.html.HTMLLabelElement;
-import sun.applet.Main;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class PlayList extends JPanel {
     private GridBagConstraints gbc;
     private JLabel yourLibrary;
-    private JLabel songs,albums,playList;
-
+    private JLabel songs, albums, playlistLabel;
+    private JList<String> playlist;
+    private DefaultListModel<String> l;
+    private JButton newPlayListButton;
     public PlayList() {
         super();
         setLayout(new GridBagLayout());
         setOpaque(true);
         setBackground(Colors.getColor("heavy grey"));
-        setPreferredSize(new Dimension(100,0));
+        setPreferredSize(new Dimension(100, 0));
         gbc = new GridBagConstraints();
+        gbc.anchor = GridBagConstraints.NORTH;
 
-        yourLibrary = makeLabelReady("YOUR LIBRARY","grey");
+        yourLibrary = makeLabelReady("YOUR LIBRARY", "grey");
+//        gbc.weightx=1;
         gbc.gridx = 0;
         gbc.gridy = 0;
         add(yourLibrary, gbc);
 
-        songs = makeLabelReady("Songs","heavy grey");
-        gbc.gridy=1;
-        add(songs,gbc);
+        songs = makeLabelReady("Songs", "heavy grey");
+        gbc.gridy = 1;
+        add(songs, gbc);
 
-         albums= makeLabelReady("Albums","heavy grey");
-         gbc.gridy=2;
-         add(albums,gbc);
+        albums = makeLabelReady("Albums", "heavy grey");
+        gbc.gridy = 2;
+        add(albums, gbc);
 
-         playList = makeLabelReady("PLAYLISTS","grey");
-         gbc.gridy=3;
-         add(playList,gbc);
+        playlistLabel = makeLabelReady("PLAYLISTS", "grey");
+        gbc.gridy = 3;
+        add(playlistLabel, gbc);
+
+        l = new DefaultListModel<>();
+        playlist = new JList<>(l);
+        //strings will be in center
+        DefaultListCellRenderer renderer = (DefaultListCellRenderer) playlist.getCellRenderer();
+        renderer.setHorizontalAlignment(JLabel.CENTER);
+
+        playlist.setPreferredSize(new Dimension(100, 600));
+        playlist.setBackground(Colors.getColor("heavy grey"));
+        playlist.setForeground(Color.WHITE);
+        l.addElement("mosa");
+        l.addElement("asdad");
+
+        gbc.gridy++;
+        gbc.fill = GridBagConstraints.VERTICAL;
+        add(playlist, gbc);
+
+        newPlayListButton = new JButton("[+]New Playlist");
+        newPlayListButton.setBackground(Colors.getColor("heavy grey"));
+        newPlayListButton.setForeground(Color.WHITE);
+        newPlayListButton.setPreferredSize(new Dimension(100,20));
+        gbc.gridy++;
+        gbc.fill=GridBagConstraints.CENTER;
+        gbc.weighty=1;
+        add(newPlayListButton,gbc);
     }
 
-    private JLabel makeLabelReady(String labelName,String colorName) {
-        JLabel label=new JLabel(labelName,SwingConstants.CENTER);
+    /*
+        @param: takes a JLabel and it's name and color and produces it.
+    */
+    private JLabel makeLabelReady(String labelName, String colorName) {
+        JLabel label = new JLabel(labelName, SwingConstants.CENTER);
         label.setOpaque(true);
         label.setBackground(Colors.getColor(colorName));
         label.setForeground(Color.WHITE);
