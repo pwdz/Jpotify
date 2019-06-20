fa560a105e432f7a17c12bb8e2f557d7399252be
taratt <51206120+taratt@users.noreply.github.com>
2019-06-21 01:19:14 +0430

    Album.java is updated

--- src/Lists/Album.java
+++ src/Lists/Album.java
@@ -1,9 +1,26 @@
 package Lists;
 
+import Music.Song;
+
+import java.util.ArrayList;
+
 public class Album extends List {
-
-
-    public Album(String name) {
+    byte artwork[];
+    public Album(String name){
         super(name);
+        setArtwork();
     }
+    @Override
+    public ArrayList<Song> getSongs(){
+        return songs;
+    }
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+    public void setArtwork(){
+        artwork=songs.get(0).getArtwork();
+    }
+
 }
+
