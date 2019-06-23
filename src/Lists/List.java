82a78338713649d4c8e5fe68caa0155dde1dcc9d
taratt <51206120+taratt@users.noreply.github.com>
2019-06-23 11:03:28 +0430

    List.java is updated with new methods( search, shuffle,...) ArrayList<Song> is changed to ArrayList<String>

--- src/Lists/List.java
+++ src/Lists/List.java
@@ -1,24 +1,71 @@
 package Lists;
 import Music.Song;
-
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 
-public class List {
-    protected ArrayList<Song> songs;
-    protected String name;
-    protected byte[] artwork;
-    public List(String name)
-    {
-        this.name=name;
-        songs=new ArrayList<>();
-    }
+    public class List {
+        protected ArrayList<String> songPath;
+        protected String name;
 
-    public String getName()
-    {
-        return name;
-    }
+        public List(String name)
+        {
+            this.name=name;
+            songPath=new ArrayList<>();
+        }
 
-    public ArrayList<Song> getSongs() {
-        return songs;
+        public String getName()
+        {
+            return name;
+        }
+
+        public ArrayList<String> getsongPath() {
+            return songPath;
+        }
+        public ArrayList<String> search(String key){
+            Song song;
+            ArrayList<String> foundItems=new ArrayList<>();
+            for (String path: songPath) {
+                try {
+                    song=new Song(path);
+                    if(song.getTitle().contains(key))
+                        foundItems.add(path);
+                    if(song.getAlbum().contains(key))
+                        foundItems.add(path);
+                    if(song.getArtist().contains(key))
+                        foundItems.add(path);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            return foundItems;
+        }
+        public ArrayList<String> shuffle(){
+            ArrayList<String> shuffledsongPath=new ArrayList<>();
+            for (int i = 0; i < songPath.size(); i++) {
+                shuffledsongPath.add(songPath.get(i));
+            }
+            Collections.shuffle(shuffledsongPath);
+            return shuffledsongPath;
+        }
+        public String nextSong(String path){
+            int index= songPath.indexOf(path);
+            if(index!=songPath.size()-1)
+                return songPath.get(index+1);
+            else
+                return songPath.get(0);
+
+        }
+        public String previousSong(String path){
+            int index=songPath.indexOf(path);
+            if(index!=0)
+                return songPath.get(index-1);
+            else
+                return songPath.get(songPath.size()-1);
+        }
+
+    public ArrayList<String> getSongPath() {
+        return songPath;
     }
 }
