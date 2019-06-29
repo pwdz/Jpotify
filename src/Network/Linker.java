15cdd82d4150ba52c22e69734308b47c678037b8
taratt <51206120+taratt@users.noreply.github.com>
2019-06-29 16:28:04 +0430

    linker updated

--- src/Network/Linker.java
+++ src/Network/Linker.java
@@ -1,4 +1,11 @@
 package Network;
 
+import Network.Server.ClientHandler;
+
+
+import java.util.HashMap;
+
+
 public interface Linker {
+    public HashMap<String,ClientHandler> getUsernamesAndClientHandlers();
 }
